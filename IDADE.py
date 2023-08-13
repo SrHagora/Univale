507585ecd06de5f8f1a7d92f19b545e4aa59a08e
SrHagora <maiocondametto@gmail.com>
2023-08-13 16:31:50 -0300

    first commit

---
+++ IDADE.py
@@ -0,0 +1,20 @@
+#variaveis 
+nome =  (input("Digite se Nome e Sobrenome:"))
+idade = int (input("Digite sua idade:"))
+altura =    (input("Digite seu Altura:"))
+pesso =    (input("Digite seu pesso:"))
+
+print("sua informações são:")
+print(nome)
+print(altura)
+print(pesso)
+
+
+
+print("sua idade e", idade,"e voce... ")
+#maior de idade
+if idade >= 18:
+    print("e maior de idade")
+else:
+    print("e menor de indade")
+    
