8acb377da75f4dcf1d0f7765421fe71baf5b7ec4
SrHagora <141462750+SrHagora@users.noreply.github.com>
2023-08-18 20:04:43 -0300

    Update Maior-valor.py

--- Maior-valor.py
+++ Maior-valor.py
@@ -2,9 +2,9 @@ valor1 = int(input('Digite o primeiro numero:'))
 valor2 = int(input('Digite o segundo numero:'))
 
 if valor1 > valor2:
-    print(f'O primeiro numero digitado ({valor1}) e o numero e maior')
+    print(f'O primeiro numero digitado ({valor1})  é o numero maior')
 if valor2 > valor1:
-    print(f'O segundo numero digitado ({valor2}) e o numero maior')
+    print(f'O segundo numero digitado ({valor2}) é o numero maior')
 else:
     print('Os valores são iguais')
 
