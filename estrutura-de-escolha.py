a3316dafcd79c5ef5d55381c534856ffb867151c
SrHagora <maicondametto@gmail.com>
2023-09-01 19:48:26 -0300

    first commit

---
+++ estrutura-de-escolha.py
@@ -0,0 +1,10 @@
+opcao = int(input('escolha uma opção:\n1 - chocolate - \n2 - coca cola - \n3 - sua mãe  '))
+
+if opcao == 1:
+    print('voce escolheu chocolate')    
+elif opcao == 2:
+    print('voce escolheu coca cola')
+elif opcao == 3:
+    print ('voce escolheu sua mãe')
+else:
+    print('opção invalida')
